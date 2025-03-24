2dc7abfd3a3786ea27a14f1bf2c36eeb25321a14
Adrian Castillo <xiuhblaze@gmail.com>
2025-03-24 17:55:14 -0600

    Archivos faltantes

---
+++ src/views/appForms/components/AppFormButtonNewItem.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+import { faWindowMaximize } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import AppFormModalEditItem from './AppFormModalEditItem';
+
+const AppFormButtonNewItem = () => {
+    const [showAppFormModal, setShowAppFormModal] = useState(false);
+
+    const onShowAppFormModal = () => {
+        console.log('onShowAppFormModal: show new app form');
+        setShowAppFormModal(true);
+    }; // onShowAppFormModal
+
+    const onHideAppFormModal = () => {
+        console.log('onHideAppFormModal: hide');
+        setShowAppFormModal(false);
+    }; // onHideAppFormModal
+
+    return (
+        <div>
+            <button
+                type="button"
+                className="btn btn-link p-0 mb-0"
+                title="New application form"
+                onClick={onShowAppFormModal}
+            >
+                <FontAwesomeIcon icon={faWindowMaximize} />
+            </button>
+            <AppFormModalEditItem 
+                show={showAppFormModal}
+                onHide={onHideAppFormModal}
+            />
+        </div>
+    )
+}
+
+export default AppFormButtonNewItem;
