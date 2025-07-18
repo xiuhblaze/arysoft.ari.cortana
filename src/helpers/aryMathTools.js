e18b0b060128b25252cd5a44201ee5b8d57ad369
Adrian Castillo <xiuhblaze@gmail.com>
2025-07-18 12:54:14 -0600

    Trabajo en aryMathTools aun no funciona correctamente

--- src/helpers/aryMathTools.js
+++ src/helpers/aryMathTools.js
@@ -9,25 +9,37 @@ const aryMathTools = () => {
      */
     const round = (value, decimals, rounding) => {
         const factor = Math.pow(10, decimals);
-console.log('round', value, factor, rounding);
-console.log('round', value * factor, value * factor / factor);
-
-
+// console.log('round props', value, factor, rounding);
+// console.log('round factor', value * factor, value * factor / factor);
+        let valRounded = value * factor;
+console.log('round.start -------');
+console.log('round.value', value);
+console.log('round.rounding', rounding);
         switch (rounding) {
             case 'up':
-                console.log('round.ceil', Math.ceil((value * factor) / factor));
-                return Math.ceil((value * factor) / factor);
+                valRounded = Math.ceil(value * factor);                
+                // const result = Math.trunc(rounded) / factor;
+                // console.log('round.ceil', Math.ceil((value * factor) / factor));
+                // return Math.ceil((value * factor) / factor);
+                break;
             case 'down':
-                console.log('round.floor', Math.floor((value * factor) / factor));
-                return Math.floor((value * factor) / factor);
+                valRounded = Math.floor(value * factor);
+                // console.log('round.floor', Math.floor((value * factor) / factor));
+                // return Math.floor((value * factor) / factor);
+                break;
             case 'half':
-                console.log('round.round', Math.round((value * factor) / factor));
-                return Math.round((value * factor) / factor);
-            
+                valRounded = Math.round(value * factor);
+                // console.log('round.round', Math.round((value * factor) / factor));
+                // return Math.round((value * factor) / factor);
+                break;
             default:
-                console.log('round.round', (value * factor) / factor);
-                return  (value * factor) / factor;
-        };
+                // console.log('round.default', (value * factor) / factor);
+                // return  (value * factor) / factor;
+                break;
+        };        
+console.log('round.valRounded', valRounded);
+console.log('round.result', Math.trunc(valRounded) / factor)
+        return Math.trunc(valRounded) / factor;
     };
 
     return {
